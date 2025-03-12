066c5797c50aa2668f45a7f717be24e4e16ab1a8
Ömer Erdem BİLGİN <o.erdem.bilgin@icisleri.gov.tr>
2025-03-12 13:00:55 +0300

    Added MySql integrations

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -7,10 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BlogContext>(options =>
 {
     ConfigurationManager config = builder.Configuration;
-    string? connectionString = config.GetConnectionString("sql_connection");
-    options.UseSqlite(connectionString);
-    // var version = new MySqlServerVersion(new Version(8, 4, 4));
-    // options.UseMySql(connectionString, version);
+    string? connectionString = config.GetConnectionString("mysql_connection");
+    // options.UseSqlite(connectionString);
+    var version = new MySqlServerVersion(new Version(8, 4, 4));
+    options.UseMySql(connectionString, version);
 });
 
 var app = builder.Build();

45fd651ef9653386cb33a2d1b3b6463af35e710a
Ömer Erdem BİLGİN <o.erdem.bilgin@icisleri.gov.tr>
2025-03-12 12:58:57 +0300

    Created and seeded database

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,5 +1,21 @@
+using BlogApp.Data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<BlogContext>(options =>
+{
+    ConfigurationManager config = builder.Configuration;
+    string? connectionString = config.GetConnectionString("sql_connection");
+    options.UseSqlite(connectionString);
+    // var version = new MySqlServerVersion(new Version(8, 4, 4));
+    // options.UseMySql(connectionString, version);
+});
+
 var app = builder.Build();
+
+SeedData.SeedTestDatas(app);
 
 app.MapGet("/", () => "Hello World!");
 

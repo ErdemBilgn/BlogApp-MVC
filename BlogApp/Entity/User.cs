71b2db9936c09b686fd34a53ce0c2707f3a056d7
Ömer Erdem BİLGİN <o.erdem.bilgin@icisleri.gov.tr>
2025-03-12 11:43:04 +0300

    Created Entitites

---
+++ BlogApp/Entity/User.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BlogApp.Entity;
+
+public class User
+{
+    public int UserId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public List<Post> Posts { get; set; } = [];
+
+    public List<Comment> Comments { get; set; } = [];
+}

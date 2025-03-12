cfa9a3b957ef2897af1300242d8c24093b3e3b12
Ömer Erdem BİLGİN <o.erdem.bilgin@icisleri.gov.tr>
2025-03-12 13:41:47 +0300

    Added bootstrap libraries

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddScoped<IPostRepository, EfPostRepository>();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 SeedData.SeedTestDatas(app);
 
 app.MapDefaultControllerRoute();
